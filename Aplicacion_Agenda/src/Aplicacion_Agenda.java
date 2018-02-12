34f27b0b3ef8c2bf8d81375a2a0ef58f15c49cd7
Bienvenidos <Bienvenidos@usuario>
2018-02-12 18:30:02 +0100

    Tercer commit

--- Aplicacion_Agenda/src/Aplicacion_Agenda.java
+++ Aplicacion_Agenda/src/Aplicacion_Agenda.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 public class Aplicacion_Agenda {
 
     static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
         Calendar fecha;
         int opcion;
@@ -23,59 +24,124 @@ public class Aplicacion_Agenda {
         Cita cita;
         int hora, minutos, dia, mes, año;
         String texto, titulo;
-        
-        
+
         System.out.println("En que año estara ubicada la agenda?");
-        año=teclado.nextInt();
-        agenda=new Agenda(año);
-        System.out.println("\n\n"+"*********CITAS****DEL****"+agenda.getAño()+"*********");
-        
+        año = teclado.nextInt();
+        agenda = new Agenda(año);
+        System.out.println("\n\n" + "*********CITAS****DEL****" + agenda.getAño() + "*********");
+
         int elec;
         do {
             verMenu();
             elec = teclado.nextInt();
-            
+
             switch (elec) {
                 case 1:
-                    //Añadir los metodos para validar que el dia y el mes son correctos(Crear metodos!!(Estaticos en Aplacacion_Agenda))
                     System.out.print("Introduce dia: ");
-                    dia=teclado.nextInt();
+                    dia = teclado.nextInt();
                     System.out.print("Introduce mes: ");
-                    mes=teclado.nextInt();
-                    agenda.abierta=agenda.buscarPagina(dia, mes);
-                    System.out.println("La pagina seleccionada es: "+agenda.abierta.getDia()+" del mes "+agenda.abierta.getMes());
-                    
+                    mes = teclado.nextInt();
+                    if (validarCita(dia, mes)) {
+                        agenda.abierta = agenda.buscarPagina(dia, mes);
+                        System.out.println("La pagina seleccionada es: " + agenda.abierta.getDia() + " del mes " + agenda.abierta.getMes());
+                    } else {
+                        System.out.println("La fecha es erronea.");
+                    }
+
                     break;
                 case 2:
-                    //Validar horas y minutos
                     System.out.print("Introduca hora: ");
-                    hora=teclado.nextInt();
+                    hora = teclado.nextInt();
                     System.out.print("Introduce minutos: ");
-                    minutos=teclado.nextInt();
-                    System.out.print("Introduce titulo: ");
-                    titulo=teclado.next();
-                    System.out.println("Introduce texto: ");
-                    texto=teclado.next();
-                    //confirmar si la hora de la cita esta ya ocupada
-                    cita=new Cita(hora, minutos, titulo, texto);
-                    agenda.abierta.añadirCita(cita);
-                    agenda.abierta.leerPagina();
-                    
+                    minutos = teclado.nextInt();
+                    if (validarHora(hora, minutos)) {
+                        System.out.print("Introduce titulo: ");
+                        titulo = teclado.next();
+                        System.out.println("Introduce texto: ");
+                        texto = teclado.next();
+                        if (agenda.abierta.agendaLibre(hora, minutos)) {
+                            cita = new Cita(hora, minutos, titulo, texto);
+                            agenda.abierta.añadirCita(cita);
+                            agenda.abierta.leerPagina();
+                        } else {
+                            System.out.println("Su agenda ya esta ocupada a esa hora");
+                        }
+                    } else {
+                        System.out.println("La hora introducida no es correcta.");
+                    }
+
                     break;
                 case 3:
+                    System.out.println("Que hora tiene la cita?");
+                    hora= teclado.nextInt();
+                    System.out.println("Que minutos tiene la cita?");
+                    minutos=teclado.nextInt();
+                    if(agenda.abierta.agendaLibre(hora, minutos)){
+                        System.out.println("No tiene ninguna cita a esa hora.");
+                    }else{
+                        agenda.abierta.borrarAgenda(hora, minutos);
+                        System.out.println("Cita borrada.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Que hora tiene la cita?");
+                    hora= teclado.nextInt();
+                    System.out.println("Que minutos tiene la cita?");
+                    minutos=teclado.nextInt();
+                    if(agenda.abierta.agendaLibre(hora, minutos)){
+                        System.out.println("No tiene ninguna cita a esa hora.");
+                    }else{
+                        agenda.abierta.buscarCita(hora, minutos);
+                        agenda.abierta.leerCitaEspecifica(hora, minutos);
+                    }
+                    
                     break;
                 default:
                     break;
             }
-        } while (elec != 0);      
+        } while (elec != 0);
     }
+
     public static void verMenu() {
         System.out.println("****************MENU****************");
         System.out.println("1. Selecciona una página");
         System.out.println("2. Crea una cita");
-        
+        System.out.println("3. Borrar cita");
+        System.out.println("4. Buscar cita");
+
         System.out.println("0. Salir");
         System.out.println("************************************");
+    }
+
+    public static boolean validarCita(int d, int m) {
+
+        if (0 < m && m <= 12) {
+            if (m == 2) {
+                if (d <= 29) {
+                    return true;
+                }
+            } else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+                if (d <= 31) {
+                    return true;
+                }
+            } else {
+                if (d <= 30) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean validarHora(int h, int m) {
+
+        if (h < 24) {
+            if (m < 60) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- Aplicacion_Agenda/src/Pagina.java
+++ Aplicacion_Agenda/src/Pagina.java
@@ -6,61 +6,97 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Bienvenidos
  */
 public class Pagina {
+
     private int dia;
     private int mes;
-    
+
     private ArrayList<Cita> citas = new ArrayList<>();
 
     public Pagina(int dia, int mes) {
         this.dia = dia;
         this.mes = mes;
     }
-    
-    public void añadirCita (Cita c){
+
+    public void añadirCita(Cita c) {
         this.citas.add(c);
     }
-    
-    public void borrarCita(Cita c){
-        
-        for(Cita cita: citas){
-            if(cita.getHora()==c.getHora() && cita.getMinutos()==c.getMinutos()){
+
+    public void borrarCita(Cita c) {
+
+        for (Cita cita : citas) {
+            if (cita.getHora() == c.getHora() && cita.getMinutos() == c.getMinutos()) {
                 this.citas.remove(cita);
             }
         }
     }
-    
-    public Cita buscarCita(int hora, int minutos){
-        
+
+    public Cita buscarCita(int hora, int minutos) {
+
         Cita busqueda = null, temp;
         int cont = 0;
         boolean encontrado = false;
-        
-        while(cont < this.citas.size() && encontrado == false){
+
+        while (cont < this.citas.size() && encontrado == false) {
             temp = this.citas.get(cont++);
-            if (temp.getHora() == hora && temp.getMinutos() == minutos){
+            if (temp.getHora() == hora && temp.getMinutos() == minutos) {
                 busqueda = temp;
                 encontrado = true;
             }
         }
         return busqueda;
     }
-    
+
     public void leerPagina() {
-        StringBuilder st = new StringBuilder (this.getDia() + "/" + this.getMes() + "\n");
-        if (this.citas.isEmpty()){
+        StringBuilder st = new StringBuilder(this.getDia() + "/" + this.getMes() + "\n");
+        if (this.citas.isEmpty()) {
             st.append("La página está en blanco.\n");
-        }else{
-            for(int i=0; i<this.citas.size(); i++){
+        } else {
+            for (int i = 0; i < this.citas.size(); i++) {
                 st.append(this.citas.get(i).leerCita());
             }
         }
         System.out.println(st.toString());
+    }
+
+    public void leerCitaEspecifica(int h, int m) {
+        StringBuilder st = new StringBuilder(this.getDia() + "/" + this.getMes() + "\n");
+        for (int i = 0; i < this.citas.size(); i++) {
+            if (this.citas.get(i).getHora() == h && this.citas.get(i).getMinutos() == m) {
+                st.append(this.citas.get(i).leerCita());
+            }
+        }
+
+        System.out.println(st.toString());
+    }
+
+    public boolean agendaLibre(int h, int m) {
+
+        Cita temp;
+        int cont = 0;
+        boolean encontrado = false;
+
+        while (cont < this.citas.size() && encontrado == false) {
+            temp = this.citas.get(cont++);
+            if (temp.getHora() == h && temp.getMinutos() == m) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void borrarAgenda(int h, int m) {
+        Cita temp;
+        for (int i = 0; i < this.citas.size(); i++) {
+            temp = this.citas.get(i);
+            if (temp.getHora() == h && temp.getMinutos() == m) {
+                this.citas.remove(i);
+            }
+        }
     }
 
     public int getDia() {
@@ -86,5 +122,5 @@ public class Pagina {
     public void setCitas(ArrayList<Cita> citas) {
         this.citas = citas;
     }
-    
+
 }
